c953bfafaf7fe0ab67ac56b1b8d086e97bbc54f7
Mostafa Jaafari <87696624+mostafa-jaafari@users.noreply.github.com>
2025-06-11 19:49:54 +0000

    .

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,5 +1,4 @@
-'use client';
-import { House, Search } from "lucide-react";
+import { BadgeHelp, Cog, House, Search, Trash2 } from "lucide-react";
 import { GlobalLogo } from "./GlobalLogo";
 import { SideBar_General } from "./SideBar_General";
 
@@ -13,13 +12,32 @@ export default function SideBar() {
             <GlobalLogo />
         </section>
         <section className="-space-y-4">
-          <ul className="px-4 py-2 flex flex-col gap-1">
+          <ul className="px-4 py-2 flex flex-col gap-1 mb-2">
             <li 
               className="flex hover:bg-neutral-800 rounded-lg cursor-pointer p-2 items-center gap-2 text-sm"><Search size={16} />Search</li>
             <li 
               className="flex hover:bg-neutral-800 rounded-lg cursor-pointer p-2 items-center gap-2 text-sm"><House size={16} />Home</li>
           </ul>
           <SideBar_General />
+        </section>
+        <section>
+          <ul className="px-4 py-2 flex flex-col gap-1 mt-2">
+            <li 
+              className="flex hover:bg-neutral-800 rounded-lg 
+                cursor-pointer p-2 items-center gap-2 text-sm">
+                  <Cog size={16} /> Settings
+            </li>
+            <li 
+              className="flex hover:bg-neutral-800 rounded-lg 
+                cursor-pointer p-2 items-center gap-2 text-sm">
+                  <BadgeHelp size={16} /> Help
+            </li>
+            <li 
+              className="flex hover:bg-red-800 rounded-lg text-red-600
+                cursor-pointer p-2 items-center gap-2 text-sm hover:text-white">
+                  <Trash2 size={16} /> Trash
+            </li>
+          </ul>
         </section>
     </main>
   );
--- src/components/UI/CustomDropDown.tsx
+++ src/components/UI/CustomDropDown.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Bubbles, ChevronDown, EllipsisVertical, Plus } from "lucide-react";
+import { Bubbles, ChevronDown, ClipboardList, EllipsisVertical, Plus } from "lucide-react";
 import React, { useEffect, useRef, useState } from "react";
 
 interface CustomDropDownProps {
@@ -7,8 +7,9 @@ interface CustomDropDownProps {
     onSelect: (option: string) => void;
     title: string;
     icon: React.ReactNode;
+    itemicon?: React.ReactNode;
 }
-export function CustomDropDown({ options, onSelect, title, icon } : CustomDropDownProps) {
+export function CustomDropDown({ options, onSelect, title, icon, itemicon } : CustomDropDownProps) {
     const [IsOpen, setIsOpen] = useState(false);
     // const dropdownRef = useRef<HTMLButtonElement>(null);
     // useEffect(() => {
@@ -27,19 +28,22 @@ export function CustomDropDown({ options, onSelect, title, icon } : CustomDropDo
             // ref={dropdownRef}
             className="text-sm p-2">
             <button 
-                onClick={() => setIsOpen(!IsOpen)}
                 className="group w-full flex items-center justify-between 
                     gap-2 px-4 py-2
                     text-white rounded-lg focus:outline-none focus:ring-1
                     focus:ring-neutral-800 hover:bg-neutral-800">
-                <span className="flex items-center gap-2 text-white">
+                <span 
+                    onClick={() => setIsOpen(!IsOpen)}
+                    className="flex w-full items-center gap-2 text-white">
                     {icon} {title}
                 </span> 
                 <div className="flex items-center gap-1">
                     <span className="p-0.5 cursor-pointer hidden group-hover:flex rounded hover:bg-neutral-700/50">
                         <Plus size={14} />
                     </span> 
-                    <span className="hover:bg-neutral-700/50 p-0.5 rounded">
+                    <span 
+                        onClick={() => setIsOpen(!IsOpen)}
+                        className="hover:bg-neutral-700/50 p-0.5 rounded">
                         <ChevronDown 
                             className={`transition-all duration-200 ${!IsOpen && "-rotate-90"}`} 
                             size={16}
@@ -55,8 +59,12 @@ export function CustomDropDown({ options, onSelect, title, icon } : CustomDropDo
                                 key={index} 
                                 className={`group w-full px-2 py-1 rounded-lg text-neutral-300
                                     hover:bg-neutral-800 cursor-pointer flex items-center justify-between ${index === 0 && "mt-2"}`}
-                                onClick={() => onSelect(option)}>
-                                {option} 
+                            >
+                                <span 
+                                    onClick={() => onSelect(option)}
+                                    className="w-full flex items-center gap-2">
+                                    <ClipboardList size={16} />{option} 
+                                </span>
                                 <span className="p-0.5 cursor-pointer hidden group-hover:block rounded hover:bg-neutral-700/50">
                                     <EllipsisVertical size={16} />
                                 </span> 
