3aa72c98c92057c0ee42c1c020fbfdc42fb250e8
Mostafa Jaafari <87696624+mostafa-jaafari@users.noreply.github.com>
2025-06-11 17:19:44 +0000

    .

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -5,7 +5,7 @@ export default function SideBar() {
   return (
     <main className="min-w-64 bg-neutral-900 rounded-lg border border-neutral-800">
         <section>
-            Test hello
+            Heeeeelo World
         </section>
     </main>
   );
